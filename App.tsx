38c233427e3232bc57ae332c888b89d61caec5b6
João Eliandro <eliandrogermano@gmail.com>
2020-06-05 21:05:23 -0300

    Add suport to google fonts into the project

--- App.tsx
+++ App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import { AppLoading } from 'expo';
+import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
+import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
 
 import Home from './src/pages/Home';
 
 export default function App() {
+  const [fontLoading] = useFonts({
+    Ubuntu_700Bold,
+    Roboto_500Medium,
+    Roboto_400Regular
+  });
+
+  if (!fontLoading) return <AppLoading />
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
